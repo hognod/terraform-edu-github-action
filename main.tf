0d57caccfd0c3c258ef0dac1d8582a313936b1f7
hognod <dongoh312@icloud.com>
2025-11-10 18:18:16 +0900

    test

--- main.tf
+++ main.tf
@@ -9,12 +9,12 @@ terraform {
   }
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = var.address_space
-  enable_dns_support = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "${var.prefix}-vpc"
-  }
-}
+#resource "aws_vpc" "main" {
+#  cidr_block = var.address_space
+#  enable_dns_support = true
+#  enable_dns_hostnames = true
+#
+#  tags = {
+#    Name = "${var.prefix}-vpc"
+#  }
+#}
